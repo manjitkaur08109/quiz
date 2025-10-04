267086774afe53b973856c46d6863a1f53c51726
RaMeSh <ramesh@eligocs.com>
2025-10-04 17:43:50 +0530

    first commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel + Vue SPA</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <div id="app"></div>
+
+</body>
+</html>
